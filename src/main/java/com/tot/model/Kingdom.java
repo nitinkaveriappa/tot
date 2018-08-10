34b53183b534239d4aa304ccbe421619f1778bfb
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@users.noreply.github.com>
2018-08-11 00:05:14 +0530

    Init

---
+++ src/main/java/com/tot/model/Kingdom.java
@@ -0,0 +1,36 @@
+package com.tot.model;
+
+public enum Kingdom {
+    LAND("PANDA"),
+    WATER("OCTOPUS"),
+    ICE("MAMMOTH"),
+    AIR("OWL"),
+    FIRE("DRAGON");
+
+    private final String emblem;
+
+    Kingdom(String emblem) {
+        this.emblem = emblem;
+    }
+
+    public String getEmblem() {
+        return emblem;
+    }
+
+    public static Kingdom getenumObject(String emblem) throws IllegalArgumentException {
+        if(emblem!=null) {
+            for(Kingdom t:Kingdom.values()) {
+                if(t.getEmblem().equals(emblem)) {
+                    return t;
+                }
+            }
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+
+        return null;
+
+    }
+
+}
---
+++ src/test/java/com/tot/model/KingdomTest.java
@@ -0,0 +1,17 @@
+package com.tot.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class KingdomTest {
+
+    @Test
+    public void KingdomTest() {
+        assertEquals(Kingdom.LAND.getEmblem(),"PANDA");
+        assertEquals(Kingdom.WATER.getEmblem(),"OCTOPUS");
+        assertEquals(Kingdom.ICE.getEmblem(),"MAMMOTH");
+        assertEquals(Kingdom.AIR.getEmblem(),"OWL");
+        assertEquals(Kingdom.FIRE.getEmblem(),"DRAGON");
+    }
+}
