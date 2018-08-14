0a1605b0e68f8a92063ae802743892b6c3b706d0
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@users.noreply.github.com>
2018-08-14 21:08:03 +0530

    getEmblem

--- src/main/java/com/tot/util/MessageUtil.java
+++ src/main/java/com/tot/util/MessageUtil.java
@@ -1,22 +1,52 @@
 package com.tot.util;
 
+import com.tot.model.Kingdom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 
 public class MessageUtil {
 
-    private String getEmblem(String kingdom) {
+    private final static Logger logger = LoggerFactory.getLogger(MessageUtil.class);
 
-        return "string";
+    private String getEmblem(String kingdom) {
+        kingdom = kingdom.toUpperCase();
+        String emblem = "";
+        switch (kingdom) {
+            case "LAND":
+                emblem = Kingdom.LAND.getEmblem();
+                break;
+            case "WATER" :
+                emblem = Kingdom.WATER.getEmblem();
+                break;
+            case "ICE" :
+                emblem = Kingdom.ICE.getEmblem();
+                break;
+            case "AIR" :
+                emblem = Kingdom.AIR.getEmblem();
+                break;
+            case "FIRE" :
+                emblem = Kingdom.FIRE.getEmblem();
+                break;
+            default:
+                break;
+        }
+        if(emblem.equals("")) {
+            return emblem;
+        }
+        else {
+            logger.info("Kingdom value Invalid");
+            return "NADA";
+        }
     }
 
     private HashMap<Character, Integer> countCharsInMessage(String msg) {
         msg = msg.toLowerCase();
         int len = msg.length();
         HashMap<Character, Integer> cntChars = new HashMap<>(Math.min(len, 26));
-
         for (int i = 0; i < len; ++i) {
             char charAt = msg.charAt(i);
-//            System.out.println(charAt);
             if (!cntChars.containsKey(charAt)) {
                 cntChars.put(charAt, 1);
             }
@@ -24,8 +54,7 @@ public class MessageUtil {
                 cntChars.put(charAt, cntChars.get(charAt) + 1);
             }
         }
-
-        System.out.println(cntChars);
+        logger.info(cntChars.toString());
         return cntChars;
     }
 
@@ -44,11 +73,17 @@ public class MessageUtil {
     }
 
     public static void main(String args[]) {
-        String msg1 = "PandA";
-        String msg2 = "Die or play the tame of thrones";
+        String kingdom = "ICE";
+        String msg2 = "Ahoy! Fight for me with men and money";
         MessageUtil messageUtil = new MessageUtil();
-        HashMap<Character, Integer> secret = messageUtil.countCharsInMessage(msg1);
+
+        String emblem = messageUtil.getEmblem(kingdom);
+        System.out.println("Emblem: " + emblem);
+
+        HashMap<Character, Integer> secret = messageUtil.countCharsInMessage(emblem);
         HashMap<Character, Integer> msg = messageUtil.countCharsInMessage(msg2);
+
+
         System.out.println("Equals Method: " +secret.equals(msg));
         System.out.println("Check: " + messageUtil.checkSecretExists(secret, msg));
     }
